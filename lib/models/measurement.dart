9c0a6daf36fc040db45d98e0b626761b70e93856
gl4a-dev <gl4aveloso.dev@gmail.com>
2026-08-29 21:29:31 -0300

    feat: new measurement representations to interact with storage

--- lib/models/measurement.dart
+++ lib/models/measurement.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'location.dart';
 import 'internet_quality.dart';
 import 'network_status.dart';
@@ -24,9 +26,49 @@ class Measurement {
 		this.noiseMeasurement,
 	});
 
+	Map<String, dynamic> toMap() {
+		return {
+			'id': id,
+			'timestamp': timestamp.toIso8601String(),
+			'location': location?.toMap(),
+			'internet_quality': internetQuality?.toMap(),
+			'network_status': networkStatus?.toMap(),
+			'wifi_list': wifiList?.toMapList(),
+			'noise_measurement': noiseMeasurement?.toMap(),
+		};
+	}
+
+	String toJsonString() => jsonEncode(toMap());
+
+	factory Measurement.fromMap(Map<String, dynamic> map) {
+		return Measurement(
+		id: map['id'] as String,
+		timestamp: DateTime.parse(map['timestamp'] as String),
+		location: map['location'] != null
+			? Location.fromMap(map['location'] as Map<String, dynamic>)
+			: null,
+		internetQuality: map['internet_quality'] != null
+			? InternetQuality.fromMap(map['internet_quality'] as Map<String, dynamic>)
+			: null,
+		networkStatus: map['network_status'] != null
+			? NetworkStatus.fromMap(map['network_status'] as Map<String, dynamic>)
+			: null,
+		wifiList: map['wifi_list'] != null
+			? WifiList.fromMapList(map['wifi_list'] as List<dynamic>)
+			: null,
+		noiseMeasurement: map['noise_measurement'] != null
+			? NoiseMeasurement.fromMap(map['noise_measurement'] as Map<String, dynamic>)
+			: null,
+		);
+	}
+
+	factory Measurement.fromJsonString(String source) =>
+		Measurement.fromMap(jsonDecode(source) as Map<String, dynamic>);
+
 	@override
 	String toString() {
-		return '''
+		return 
+'''
 ------ MEASUREMENT ------
 $id
 $timestamp
