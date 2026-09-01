b7e9f608f98b998fa4040bc0938b3754e6e0fc15
gl4a-dev <gl4aveloso.dev@gmail.com>
2026-09-01 13:11:16 -0300

    fix: change the network status dependency to a new plugin

--- lib/services/network_status_service.dart
+++ lib/services/network_status_service.dart
@@ -1,4 +1,4 @@
-import 'package:connectivity_control/connectivity_control.dart' as connectivity_control;
+import 'dart:io';
 import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:mobile_operator_info/mobile_operator_info.dart';
 import 'package:network_info_plus/network_info_plus.dart';
@@ -9,13 +9,9 @@ import '../models/network_status.dart';
 class NetworkStatusService {
 	final Connectivity _connectivity = Connectivity();
 	final NetworkInfo _networkInfo = NetworkInfo();
-
-	final connectivity_control.ConnectivityControl _connectivityControl = connectivity_control.ConnectivityControl();
-
 	final MobileOperatorInfo _mobileOperatorInfo = MobileOperatorInfo();
 
 	Future<NetworkStatus> getCurrentNetworkStatus() async {
-
 		final connectivity = await _connectivity.checkConnectivity();
 
 		String connectionType = 'none';
@@ -36,49 +32,30 @@ class NetworkStatusService {
 			bssid = await _networkInfo.getWifiBSSID();
 		}
 
-		final activeNetworks = await _connectivityControl.getActiveNetworks();
-
-		connectivity_control.NetworkInfo? activeNetwork;
-
-		if (activeNetworks.isNotEmpty) {
-
-			for (final network in activeNetworks) {
-				final type = network.type.name;
-
-				if (connectionType == 'wifi' && type == 'wifi') {
-					activeNetwork = network;
-					break;
-				}
-
-				if (connectionType == 'mobile' && type == 'cellular') {
-					activeNetwork = network;
-					break;
-				}
-
-				if (connectionType == 'ethernet' && type == 'ethernet') {
-					activeNetwork = network;
-					break;
-				}
+		bool hasInternet = false;
+		if (connectionType != 'none') {
+			try {
+				final result = await InternetAddress.lookup('one.one.one.one')
+					.timeout(const Duration(seconds: 3));
+				hasInternet = result.isNotEmpty && result[0].rawAddress.isNotEmpty;
+			} catch (_) {
+				hasInternet = false;
 			}
-
-			activeNetwork ??= activeNetworks.first;
 		}
-
-		final hasInternet = activeNetwork?.hasInternet ?? false;
-		final isValidated = activeNetwork?.isValidated ?? false;
-		final isMetered = activeNetwork?.isMetered ?? false;
-
 
 		String? mobileOperator;
 		String? mobileCountryCode;
 		String? mobileNetworkCode;
 
 		if (connectionType == 'mobile') {
-			final operatorInfo = await _mobileOperatorInfo.getMobileOperatorInfo();
-
-			mobileOperator = operatorInfo.networkOperatorName;
-			mobileCountryCode = operatorInfo.mobileCountryCode;
-			mobileNetworkCode = operatorInfo.mobileNetworkCode;
+			try {
+				final operatorInfo = await _mobileOperatorInfo.getMobileOperatorInfo();
+				mobileOperator = operatorInfo.networkOperatorName;
+				mobileCountryCode = operatorInfo.mobileCountryCode;
+				mobileNetworkCode = operatorInfo.mobileNetworkCode;
+			} catch (_) {
+				// pass
+			}
 		}
 
 
@@ -86,9 +63,9 @@ class NetworkStatusService {
 			connectionType: connectionType,
 			connectedSsid: ssid,
 			connectedBssid: bssid,
-			isMetered: isMetered,
+			isMetered: connectionType == 'mobile',
 			hasInternet: hasInternet,
-			isValidated: isValidated,
+			isValidated: hasInternet,
 			mobileOperator: mobileOperator,
 			mobileCountryCode: mobileCountryCode,
 			mobileNetworkCode: mobileNetworkCode,
